4f3191cfb71d2170f2a06ab600dd524f50cab07f
Tai <nguyentaijs@gmail.com>
2018-11-26 17:56:34 +0700

    init commit

---
+++ src/main/java/tafi/castisutils/CastisUtilsApplication.java
@@ -0,0 +1,12 @@
+package tafi.castisutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastisUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastisUtilsApplication.class, args);
+	}
+}
---
+++ src/test/java/tafi/castisutils/CastisUtilsApplicationTests.java
@@ -0,0 +1,16 @@
+package tafi.castisutils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CastisUtilsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
